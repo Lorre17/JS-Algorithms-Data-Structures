ac298ef55f239e2fe63eee8ad4779630b879c7f7
Lorena Strechie <lorena.strechie@yahoo.com>
2020-01-21 16:48:58 +0000

    Added revString solution

--- Beginner/reverseString/index-START.js
+++ Beginner/reverseString/index-START.js
@@ -3,12 +3,32 @@ Given a string of text, write an algorithm that returns the text received in a r
 E.g reverseString('algorithms') // should return 'smhtirogla'
 */
 
-
-
-function reverseString(text) {
-    // Code goes here
+function reverseStringJSWay(text){
+    return [...text].reverse().join('')
 }
 
+function reverseStringFPWay(text){
+    const result = ""
 
+    for (let i = text.length - 1; i >= 0; i--) {
+        result += text[i]
+    }
 
-module.exports = reverseString
+    return result
+}
+
+function reverseStringFPES6Way(text) {
+  let result = "";
+
+  for (let char of text) result = char + result;
+
+  return result;
+}
+
+function reverseString(text){
+    if(text === "")
+        return ""
+    else return reverseString(text.substr(1)) + text[0]
+}
+
+module.exports = reverseString;
